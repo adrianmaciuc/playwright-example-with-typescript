75fe43bcae4fd0b829792c4eb0c96e8d1f6668f5
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-10-22 00:31:04 +0300

    lets go back to peaceiris

--- tests/buyAProduct.spec.ts
+++ tests/buyAProduct.spec.ts
@@ -1,22 +1,22 @@
 import { test, expect } from '../fixtures/basePage'
 
 
-test('Buy a product | @e2e', async ({ homePage, shippingPage, productPage }) => {
+// test('Buy a product | @e2e', async ({ homePage, shippingPage, productPage }) => {
 
-  await homePage.goto()
-  await homePage.productItem().nth(0).click()
+//   await homePage.goto()
+//   await homePage.productItem().nth(0).click()
 
-  await productPage.waitForPageToLoad()
-  await productPage.chooseSize('s')
-  await productPage.chooseColor('orange')
-  await productPage.ADD_TO_CART_BTN().click()
-  await expect(productPage.ADD_TO_CART_SUCCESS_MSG()).toBeChecked()
+//   await productPage.waitForPageToLoad()
+//   await productPage.chooseSize('s')
+//   await productPage.chooseColor('orange')
+//   await productPage.ADD_TO_CART_BTN().click()
+//   await expect(productPage.ADD_TO_CART_SUCCESS_MSG()).toBeChecked()
 
-  await productPage.proceedToCheckout()
+//   await productPage.proceedToCheckout()
 
-  await shippingPage.waitForPageToLoad()
+//   await shippingPage.waitForPageToLoad()
 
-});
+// });
 
 test('Extra test just for Report 1 | @e2e', async ({ homePage }) => {
 
