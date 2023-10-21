c9fd59ca6c8328cf72ed4b7f53b580983331d251
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-10-22 02:51:58 +0300

    add list to reporter on CI as well

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [['blob'],['github']] : 'blob',
+  reporter: process.env.CI ? [['blob'],['github'],['list']] : 'blob',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     screenshot: 'only-on-failure',
