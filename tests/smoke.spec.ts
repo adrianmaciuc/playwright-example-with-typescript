57b2304ac4a6eed4507076ae071b7f8bec1a190c
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-11-26 01:15:24 +0200

    add more failed tests

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -27,6 +27,21 @@ test("Extra test TO FAIL just for Report 3 | @e2e", async ({ request }) => {
   expect(response.status()).toEqual(201);
 });
 
+test("One more Extra test TO FAIL just for Report 5 | @e2e", async ({
+  request,
+}) => {
+  allure.suite("Smoke suite");
+  allure.owner("Adrian Maciuc");
+  allure.tags("Smoke", "Regression");
+  allure.issue("JIRA-123 link", "https://www.google.com");
+  allure.parameter("username", "<process.env.username type of injection>");
+
+  const response = await request.get(
+    "https://magento.softwaretestingboard.com/"
+  );
+  expect(response.status()).toEqual(203);
+});
+
 test("Extra test TO FAIL just for Report 4 | @e2e", async ({ request }) => {
   allure.suite("Smoke suite");
   allure.owner("Adrian Maciuc");
