f764a0643f6eceb60a00e4c5db64fd14c7d58468
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-11-25 23:56:03 +0200

    add metadata allure

--- tests/buyAProduct.spec.ts
+++ tests/buyAProduct.spec.ts
@@ -1,10 +1,16 @@
 import { test, expect } from "../fixtures/basePage";
+import { allure } from "allure-playwright";
 
 test("Buy a product | @e2e", async ({
   homePage,
   shippingPage,
   productPage,
 }) => {
+  allure.suite("Accounts suite");
+  allure.owner("Adrian Maciuc");
+  allure.tags("Smoke", "Regression");
+  allure.issue("JIRA-123 link", "https://www.google.com");
+  allure.parameter("username", "<process.env.username type of injection>");
   await homePage.goto();
   await homePage.productItem().nth(0).click();
 
@@ -20,10 +26,22 @@ test("Buy a product | @e2e", async ({
 });
 
 test("Extra test TO FAIL just for Report 1 | @e2e", async ({ homePage }) => {
+  allure.suite("Accounts suite");
+  allure.owner("Adrian Maciuc");
+  allure.tags("Smoke", "Regression");
+  allure.issue("JIRA-123 link", "https://www.google.com");
+  allure.parameter("username", "<process.env.username type of injection>");
+
   await homePage.goto();
   await expect(homePage.productItem().nth(0).isVisible()).toBeFalsy();
 });
 
 test("Extra test just for Report 2 | @e2e", async ({ homePage }) => {
+  allure.suite("Accounts suite");
+  allure.owner("Adrian Maciuc");
+  allure.tags("Smoke", "Regression");
+  allure.issue("JIRA-123 link", "https://www.google.com");
+  allure.parameter("username", "<process.env.username type of injection>");
+
   await expect(homePage.productItem().nth(0).isVisible()).toBeTruthy();
 });
--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,6 +1,13 @@
 import { test, expect } from "../fixtures/basePage";
+import { allure } from "allure-playwright";
 
 test("Test just to pass | @e2e", async ({ homePage }) => {
+  allure.suite("Smoke suite");
+  allure.owner("Adrian Maciuc");
+  allure.tags("Smoke", "Regression");
+  allure.issue("JIRA-123 link", "https://www.google.com");
+  allure.parameter("username", "<process.env.username type of injection>");
+
   await homePage.goto();
   await expect(homePage.page).toHaveURL(
     "https://magento.softwaretestingboard.com/"
@@ -8,6 +15,12 @@ test("Test just to pass | @e2e", async ({ homePage }) => {
 });
 
 test("Extra test TO FAIL just for Report 3 | @e2e", async ({ request }) => {
+  allure.suite("Smoke suite");
+  allure.owner("Adrian Maciuc");
+  allure.tags("Smoke", "Regression");
+  allure.issue("JIRA-123 link", "https://www.google.com");
+  allure.parameter("username", "<process.env.username type of injection>");
+
   const response = await request.get(
     "https://magento.softwaretestingboard.com/"
   );
@@ -15,6 +28,12 @@ test("Extra test TO FAIL just for Report 3 | @e2e", async ({ request }) => {
 });
 
 test("Extra test TO FAIL just for Report 4 | @e2e", async ({ request }) => {
+  allure.suite("Smoke suite");
+  allure.owner("Adrian Maciuc");
+  allure.tags("Smoke", "Regression");
+  allure.issue("JIRA-123 link", "https://www.google.com");
+  allure.parameter("username", "<process.env.username type of injection>");
+
   const response = await request.get(
     "https://magento.softwaretestingboard.com/"
   );
